7ee39a58026b27d144290f3d3d89e77918586993
deogos <diogo.alonso@etec.sp.gov.br>
2022-09-06 08:31:58 -0300

    Adição main activity
    - Criação da main acitivity
    - Import de todas as imagens que serão usadas no projeto
    - Alteração no design da main activity
    - Adição da activity questao1

---
+++ app/src/main/java/com/example/appquiz3bimestre/Question1.java
@@ -0,0 +1,15 @@
+package com.example.appquiz3bimestre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class Question1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+    }
+}
